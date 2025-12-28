ce30ea85476992a3dbcb0be2979ce2632cbe073e
Siarhei Kazakevich <dianrom@tut.by>
2025-12-28 18:49:11 +0300

    feat: add first text on first page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,31 @@
 import { store } from './state/store';
 
 
+const root = document.getElementById('app') as HTMLElement;
+
+
+function render(): void {
+  const { currentPage } = store.getState();
+
+  root.innerHTML = '';
+
+  if (currentPage === 'login') {
+    const title = document.createElement('h1');
+    title.textContent = 'RSS Puzzle — Login';
+
+    const subtitle = document.createElement('p');
+    subtitle.textContent = 'Введите имя, чтобы начать игру';
+
+    root.append(title, subtitle);
+  }
+}
+store.subscribe(render);
+
+// первый рендер при старте приложения
+render();
+
+
+
 store.subscribe(() => {
   // eslint-disable-next-line no-console
   console.log('STATE UPDATED:', store.getState());
